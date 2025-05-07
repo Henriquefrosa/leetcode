7b14fade8c6b9e919301ec2bd1b849eebad36479
Henrique <henrique.francisco.rosa2@gmail.com>
2025-05-07 19:30:12 -0300

    Exercicio feito

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import static java.lang.Character.getNumericValue;
+
 class Solution {
     //ListNode
     public void addTwoNumbers(ListNode l1, ListNode l2) {
@@ -33,8 +35,15 @@ class Solution {
         System.out.println(numeros[0] + " " + numeros[1]);
         num1 = Integer.parseInt(numeros[0]);
         num0 = Integer.parseInt(numeros[1]);
-
-
+        System.out.println("Soma = " + (num1+num0));
+        ListNode resultado1 = criar(String.valueOf(num1+num0));
+        for(int i = 0; i < 1000; i ++) {
+            System.out.println("Valor do ListNode: " + resultado1.val);
+            if(resultado1.next == null){
+                break;
+            }
+            resultado1 = resultado1.next;
+        }
 
     }
 
@@ -47,9 +56,13 @@ class Solution {
     }
 
     public ListNode criar(String teste){
-        for(int i = teste.length() - 1; i >= 0; i--){
-
+        ArrayList<ListNode> result = new ArrayList<ListNode>();
+        char t = teste.charAt(teste.length()-1);
+        result.add(new ListNode(Character.getNumericValue(t)));
+        for(int i = teste.length() - 2; i >= 0; i--){
+            char a = teste.charAt(i);
+            result.add( new ListNode(Character.getNumericValue(a), result.get((teste.length() - (i) - 2))));
         }
-        return new ListNode(9);
+        return result.get(teste.length()-1);
     }
 }
